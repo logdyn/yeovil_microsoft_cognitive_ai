7bb1d7c975fb2172a1a30ad2b8de3dd40843db5a
jsjlewis96 <jsjlewis96@gmail.com>
2017-03-29 19:14:23 +0100

    fixed javascript with getting data uri, need to encode?

--- ymca/src/main/webapp/js/webcam.js
+++ ymca/src/main/webapp/js/webcam.js
@@ -1,39 +1,39 @@
 var webcam = {
 	video : null,
-	init: function() 
-	{
+	init : function() {
 		video = document.querySelector(".videoElement");
-	
+
 		navigator.getUserMedia = navigator.getUserMedia
 				|| navigator.webkitGetUserMedia || navigator.mozGetUserMedia
 				|| navigator.msGetUserMedia || navigator.oGetUserMedia;
-	
-		if (navigator.getUserMedia)
-		{
-			navigator.getUserMedia(
-				{video : true},
-				webcam.handleVideo,
-				webcam.videoError);
+
+		if (navigator.getUserMedia) {
+			navigator.getUserMedia({
+				video : true
+			}, webcam.handleVideo, webcam.videoError);
 		}
 	},
-	
-	handleVideo: function(stream)
-	{
+
+	handleVideo : function(stream) {
 		video.src = window.URL.createObjectURL(stream);
 	},
-	
-	videoError: function(e)
-	{
+
+	videoError : function(e) {
 		// do something
 	},
-	
-	captureImage: function() {
-		  var canvas = document.createElement("canvas");
-		  var context = canvas.getContext("2d");
-		  canvas.width = 160;
-		  canvas.height = 90;
-		  context.drawImage(video, 0, 0, 160, 90);
-		  xhttp.sendRequest('webcamImage=' + canvas.toDataURL(), null, "WebcamServlet");
+
+	//FOR THE LOVE OF GOD DON'T TOUCH IT, IT WORKS
+	captureImage : function() {
+		var canvas = document.createElement("canvas");
+		video = document.querySelector(".videoElement");
+		canvas.height = video.videoHeight;
+		canvas.width = video.videoWidth;
+		var ctx = canvas.getContext("2d");
+		ctx.drawImage(video, 10, 10);
+		var dataURL = canvas.toDataURL();
+		console.log(dataURL);
+		xhttp.sendRequest('webcamImage=' + dataURL, null,
+				"WebcamServlet");
 	}
 }
 document.addEventListener('DOMContentLoaded', webcam.init, false);
