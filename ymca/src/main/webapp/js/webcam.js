b67a18791871906950646af8571c23bef46b4643
Matt Rayner <me@mattihew.com>
2017-03-29 14:47:51 +0100

    #36: made javascript gooder

--- ymca/src/main/webapp/js/webcam.js
+++ ymca/src/main/webapp/js/webcam.js
@@ -1,22 +1,30 @@
-var video
-document.addEventListener('DOMContentLoaded', function() {
-	video = document.querySelector(".videoElement");
-
-	navigator.getUserMedia = navigator.getUserMedia
-			|| navigator.webkitGetUserMedia || navigator.mozGetUserMedia
-			|| navigator.msGetUserMedia || navigator.oGetUserMedia;
-
-	if (navigator.getUserMedia) {
-		navigator.getUserMedia({
-			video : true
-		}, handleVideo, videoError);
+var webcam = {
+	video : null,
+	init: function() 
+	{
+		video = document.querySelector(".videoElement");
+	
+		navigator.getUserMedia = navigator.getUserMedia
+				|| navigator.webkitGetUserMedia || navigator.mozGetUserMedia
+				|| navigator.msGetUserMedia || navigator.oGetUserMedia;
+	
+		if (navigator.getUserMedia)
+		{
+			navigator.getUserMedia(
+				{video : true},
+				webcam.handleVideo,
+				webcam.videoError);
+		}
+	},
+	
+	handleVideo: function(stream)
+	{
+		video.src = window.URL.createObjectURL(stream);
+	},
+	
+	videoError: function(e)
+	{
+		// do something
 	}
-}, false);
-
-function handleVideo(stream) {
-	video.src = window.URL.createObjectURL(stream);
 }
-
-function videoError(e) {
-	// do something
-}
+document.addEventListener('DOMContentLoaded', webcam.init, false);
