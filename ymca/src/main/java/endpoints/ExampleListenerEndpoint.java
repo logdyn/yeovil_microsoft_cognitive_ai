3322b0f820b2be369a1caaa604cb8800da0d3e41
jsjlewis96 <jsjlewis96@gmail.com>
2017-03-31 12:43:46 +0100

    fixed closed session error, started implementing logger

--- ymca/src/main/java/endpoints/ExampleListenerEndpoint.java
+++ ymca/src/main/java/endpoints/ExampleListenerEndpoint.java
@@ -1,5 +1,9 @@
 package endpoints;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.websocket.CloseReason;
 import javax.websocket.Endpoint;
 import javax.websocket.EndpointConfig;
 import javax.websocket.MessageHandler;
@@ -11,6 +15,7 @@ import servlets.WebcamServlet;
 public class ExampleListenerEndpoint extends Endpoint implements Observer
 {
 	private Session session;
+	private static Logger LOGGER = Logger.getLogger(ExampleListenerEndpoint.class.getName());
 	@Override
 	public void onOpen(final Session session, final EndpointConfig ec)
 	{
@@ -20,17 +25,36 @@ public class ExampleListenerEndpoint extends Endpoint implements Observer
 			@Override
 			public void onMessage(final String text)
 			{
-				System.out.println("Added example endpoint as observer");
+				LOGGER.log(Level.FINE, "Added example endpoint as observer");
 				WebcamServlet.servletInstances.get(text).addObserver(ExampleListenerEndpoint.this);
 			}
 		});
 	}
 
+	/**
+     * Event that is triggered when a session has closed.
+     *
+     * @param session       The session
+     * @param closeReason   Why the session was closed
+     */
+    @Override
+	public void onClose(final Session session, final CloseReason closeReason) {
+    	LOGGER.log(Level.FINE, "Closed endpoint");
+    }
+	
 	@Override
 	public void update(final ObservableServlet servlet, final Object image)
 	{
 		//send image to ms
-		this.session.getAsyncRemote().sendText("Example endpoint to MS");
+		if (this.session.isOpen())
+		{
+			this.session.getAsyncRemote().sendText("Example endpoint to MS");
+			LOGGER.log(Level.FINE, "Successful update()");
+		}
+		else
+		{
+			LOGGER.log(Level.WARNING, "Attempted to use closed session");
+		}
 	}
 
 }
