7495af6502bbe4f15a12583181dee57e333ac2aa
jsjlewis96 <jsjlewis96@gmail.com>
2017-04-06 14:52:08 +0100

    Fixed display issues

--- ymca/src/main/webapp/js/outputLog.js
+++ ymca/src/main/webapp/js/outputLog.js
@@ -1,31 +1,28 @@
 var outputLog = 
 {
-		append : function(jsonMessage)
-		{
-			var logElements = Array.from(document.getElementsByClassName("logElement"));
-			logElements.forEach(
-					function(log)
-					{
-						var levelClass;
-						
-						switch(jsonMessage.level) 
-						{
-						    case 'INFO':
-						    	levelClass = "bg-info"
-						        break;
-						    case 'FINE':
-						    	levelClass = "bg-success"
-							        break;
-						    case 'WARNING':
-						    	levelClass = "bg-warning"
-						    case 'SEVERE':
-						    	levelClass = "bg-danger"
-				    		default:
-				    			levelClass = "";
-						}
-						
-						var message = '<p><samp class="' + levelClass + '">' + jsonMessage.message + '</samp></p><br/>';
-						log.innerHTML = message;
-					});
-		}
+	append : function(jsonMessage) 
+	{
+		var logElements = Array.from(document
+				.getElementsByClassName("logElement"));
+		logElements.forEach(function(log) 
+				{
+			var levelClass;
+
+			switch (jsonMessage.level) 
+			{
+				case 'FINE':
+					levelClass = "text-success"
+					break;
+				case 'SEVERE':
+					levelClass = "text-danger"
+					break;
+				default:
+					levelClass = "text-" + jsonMessage.level.toLowerCase();
+			}
+
+			var message = '<samp class="' + levelClass + '">'
+					+ jsonMessage.message + '</samp><br/>';
+			log.innerHTML += message;
+		});
+	}
 }
