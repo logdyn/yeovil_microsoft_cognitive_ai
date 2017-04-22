97e30dbe8276b7d85709ac6b7dbfe18f0e37f745
jsjlewis96 <jsjlewis96@gmail.com>
2017-04-22 16:43:03 +0100

    Initial about page work

---
+++ ymca/src/main/java/servlets/AboutServlet.java
@@ -0,0 +1,30 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AboutServlet
+ */
+public class AboutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AboutServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/pages/about.jsp").forward(request, response);
+	}
+
+}
